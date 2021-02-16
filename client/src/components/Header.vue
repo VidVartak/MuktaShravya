39bbbb2a62dac54b8792230acd0d0441d6130a1a
vid <vid@ebi.ac.uk>
2021-02-16 22:20:52 +0000

    built for production

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -11,6 +11,7 @@
       </div>
       <h1><span class="highlight"> {{$t('MuktaShravya')}} : </span> Indian Audio Books</h1>
       <h2>{{$t('headerTagLine')}}</h2>
+    </header>
 </template>
 
 <script>
